7a97a23c5e94c61dd9dc87d448c41e5694fedebd
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-05 08:54:53 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Hiiiiii</h3>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ReactDOM from "react-dom";
+import PDF from './PDF';
 
 
 function Form() {
