5e2ccadbda624f530f8b5c7adbca0bb9ea4d944d
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-05 08:42:05 -0700

    create react form component

---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+
+function Form() {
+
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.prevenDefaul();
+        alert(inputs);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Client Name
+            <input
+                type="text"
+                name="username"
+                value={inputs.username || ""}
+                onChange={handleChange}
+                />
+                </label>
+                <label>Cost of Services
+                    <input 
+                        type="number"
+                        name="cost"
+                        value={inputs.cost || ""}
+                        onChange={handleChange}
+                        />
+                </label>
+        </form>
+    )
+
+
+
+}
+
+export default Form;
