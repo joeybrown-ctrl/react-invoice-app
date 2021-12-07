a2db6718541e0af3b259cbcd76b14155d1837d57
George Joseph Brown <georgejosephbrown@Georges-iMac.local>
2021-12-06 18:29:13 -0700

    brought in invoice template and refactored heavily

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,96 +1,16 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PDF from '../PDF/PDF';
 import './Form.css';
 
 
-// class Form extends Component {
-
-//     state = {
-//         client: '',
-//         email: '',
-//         service: '',
-//         date: '',
-//         price: '',
-//         formSubmitted: false
-//     }
-
-//     onChange = input => (e) => {
-//         this.setState({
-//             [input]: e.target.value
-//         });
-//     }
-
-//     submitForm = (e) => {
-//         if (!this.state.client || !this.state.email || !this.state.service || !this.state.price) {
-//             alert('All fields are required');
-//             e.preventDefault();
-//         } else {
-//             this.setState({
-//                 formSubmitted: true
-//             })
-//         }
-//     }
-
-//     render() {
-//         return (
-//             <div>
-
-//                 {!this.state.formSubmitted ?
-//                     (<div>
-//                         <form>
-//                             <h3>Invoice to PDF</h3>
-//                             <p>Enter information in each field and click submit to preview your PDF</p>
-//                             <fieldset>
-//                                 <div>
-//                                     <label>Client:
-//                                         <input onChange={this.onChange('client')} name="client" type="text" placeholder="Enter Client Name"></input>
-//                                     </label>
-//                                 </div>
-//                                 <hr />
-//                                 <div>
-//                                     <label>Email:
-//                                         <input onChange={this.onChange('email')} name="email" type="email" placeholder="Enter Client Email"></input>
-//                                     </label>
-//                                 </div>
-//                                 <hr />
-//                                 <div>
-//                                     <label>Date:
-//                                         <input onChange={this.onChange('date')} name="Date" type="date" placeholder="Enter date"></input>
-//                                     </label>
-//                                 </div>
-//                                 <hr />
-//                                 <div>
-//                                     <label>Service:
-//                                         <input onChange={this.onChange('service')} name="service" type="text" placeholder="Service Rendered"></input>
-//                                     </label>
-//                                 </div>
-//                                 <hr />
-//                                 <div>
-//                                     <label>Price:
-//                                         <input onChange={this.onChange('price')} name="price" type="text" placeholder="Enter Price"></input>
-//                                     </label>
-//                                 </div>
-//                                 <hr />
-//                                 <div>
-//                                     <button type="button" onClick={this.submitForm}>Submit</button>
-//                                 </div>
-//                             </fieldset>
-//                         </form>
-//                     </div>) : (
-//                         <PDF client={this.state.client} email={this.state.email} service={this.state.service} date={this.state.date} price={this.state.price} />
-//                     )}
-//             </div>
-//         )
-//     }
-
-
-// }
 const Form = (props) => {
-    const [state, setState] = React.useState({
+    const [state, setState] = useState({
         client: '',
         email: '',
         service: '',
         date: '',
+        invoice: '',
+        payment: '',
         price: '',
         formSubmitted: false
     })
@@ -103,7 +23,7 @@ const Form = (props) => {
     }
 
     const submitForm = (e) => {
-        if (!state.client || !state.email || !state.service || !state.price) {
+        if (!state.client || !state.email || !state.service || !state.date || !state.invoice || !state.payment || !state.price) {
             alert('All fields are required');
             e.preventDefault();
         } else {
@@ -148,6 +68,18 @@ const Form = (props) => {
                                 </div>
                                 <hr />
                                 <div>
+                                    <label>Invoice Number:
+                                        <input onChange={onChange('invoice')} name="invoice" type="number" placeholder="Enter Invoice#"></input>
+                                    </label>
+                                </div>
+                                <hr />
+                                <div>
+                                    <label>Payment Method:
+                                        <input onChange={onChange('payment')} name="payment" type="text" placeholder="Card or Check?"></input>
+                                    </label>
+                                </div>
+                                <hr />
+                                <div>
                                     <label>Price:
                                         <input onChange={onChange('price')} name="price" type="text" placeholder="Enter Price"></input>
                                     </label>
@@ -159,7 +91,7 @@ const Form = (props) => {
                             </fieldset>
                         </form>
                     </div>) : (
-                        <PDF client={state.client} email={state.email} service={state.service} date={state.date} price={state.price} />
+                        <PDF client={state.client} email={state.email} service={state.service} date={state.date} invoice={state.invoice} payment={state.payment} price={state.price} />
                     )}
             </div>
         )
--- src/components/PDF/PDF.js
+++ src/components/PDF/PDF.js
@@ -5,25 +5,120 @@ import './PDF.css';
 
 
 
+// const PDF = (props) => {
+//     const ref = useRef();
+
+//     return (
+//         <div>
+//             <div size="A4" ref={ref} style={{width: 500, height: 500, color: "black", margin: "auto"}} >
+//                 <h3>{props.client}</h3>
+//                 <p>{props.email}</p>
+//                 <p>{props.service}</p>
+//                 <p>{props.date}</p>
+//                 <p>{props.price}</p>
+//             </div>
+//             <Pdf targetRef={ref} filename="invoice.pdf">
+//                 {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
+//                 {/* <div sref={ref}/> */}
+//             </Pdf>
+//         </div>
+//     )
+// }
+
+const options = {
+    orientation: 'portrait',
+    unit: 'in',
+    format: [4,2]
+};
+
 const PDF = (props) => {
     const ref = useRef();
-    
+
     return (
         <div>
-            <div size="A4" className="form" ref={ref} style={{width: 500, height: 500}} >
-                <h3>{props.client}</h3>
+            <div size="A4" ref={ref} style={{width: 800, height: 500, color: "black", margin: "auto"}} options={options} >
+                {/* <h3>{props.client}</h3>
                 <p>{props.email}</p>
                 <p>{props.service}</p>
                 <p>{props.date}</p>
-                <p>{props.price}</p>
+                <p>{props.price}</p> */}
+
+
+                <div className="invoice-box">
+                    <table cellpadding="0" cellspacing="0">
+                        <tr className="top">
+                            <td colspan="2">
+                                <table>
+                                    <tr>
+
+                                        <td>
+                                            Invoice #: {props.invoice}<br />
+                                            Date: {props.date}<br />
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+
+                        <tr className="information">
+                            <td colspan="2">
+                                <table>
+                                    <tr>
+                                        <td>
+                                            My Business, Inc.<br />
+                                            My Address<br />
+                                            Somewhere, AZ 12345
+                                        </td>
+
+                                        <td>
+                                            {props.client}<br />
+                                            {props.email}
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+
+                        <tr className="heading">
+                            <td>Payment Method</td>
+
+                            {/* <td>Check #</td> */}
+                        </tr>
+
+                        <tr className="details">
+                            <td>{props.payment}</td>
+
+                            {/* <td>1000</td> */}
+                        </tr>
+
+                        <tr className="heading">
+                            <td>Service</td>
+
+                            <td>Price</td>
+                        </tr>
+
+                        <tr className="item">
+                            <td>{props.service}</td>
+
+                            <td>{props.price}</td>
+                        </tr>
+
+                        <tr className="total">
+                            <td></td>
+
+                            <td>Total: {props.price}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
             <Pdf targetRef={ref} filename="invoice.pdf">
                 {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
-                {/* <div sref={ref}/> */}
             </Pdf>
         </div>
     )
 }
+
+
 
 export default PDF;
 
